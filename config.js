29b72fab78f22f7f98a5ad53bbc19f0dfee61a2e
lanhuong1911 <86755539+lanhuong1911@users.noreply.github.com>
2021-07-01 18:27:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bae à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Lạc vào tim chị lẻ loi
+    Đằng sau chữ thích đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng em đã iu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'Vẫn cứ là thích chị <33',
+    btnNo: 'Không, chị gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích chị <3',
+    btnReply: 'Gửi cho chị iu <3',
+    reply: 'Thích thì thích mà không thích thì thích <33333333',
+    mess: 'Chị biết mà 🥰. Iu em nhiều nhiều 😘😘',
+    messDesc: 'Trà sữa hay trà đào mà trà nào chả ở quán. Mai đi chơi nhé bae',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://www.facebook.com/messages/t/100053308489867/' //
